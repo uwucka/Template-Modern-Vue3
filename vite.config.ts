8577ccd343e59d19b1fff6e6ef7ff88e39bef3e5
Evgeny <53271454+uwucka@users.noreply.github.com>
2024-01-09 18:05:12 +0300

    update packages & yarn

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig, loadEnv } from 'vite'
 import path from 'path'
 import eslint from 'vite-plugin-eslint'
-import StylelintPlugin from 'vite-plugin-stylelint'
+import stylelint from 'vite-plugin-stylelint'
 import vue from '@vitejs/plugin-vue'
-import fs from 'fs'
+// import fs from 'fs'
 
 const esLintConfig = eslint({
   fix: true,
   cache: false
 })
 
-const styleLintConfig = StylelintPlugin({
+const styleLintConfig = stylelint({
   fix: true,
   cache: false
 })
@@ -20,12 +20,12 @@ export default ({ mode }) => {
 
   return defineConfig({
     // Delete the server if you do not use https
-    server: {
-      https: {
-        key: fs.readFileSync(process.env.VITE_SSL_KEY_FILE),
-        cert: fs.readFileSync(process.env.VITE_SSL_CRT_FILE)
-      }
-    },
+    // server: {
+    //   https: {
+    //     key: fs.readFileSync(process.env.VITE_SSL_KEY_FILE),
+    //     cert: fs.readFileSync(process.env.VITE_SSL_CRT_FILE)
+    //   }
+    // },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, 'src')
